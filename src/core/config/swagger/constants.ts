efad46c413559747aef6bfeecc16810707818e2a
baradd <mohammadtavassolian3@gmail.com>
2026-06-07 18:24:11 +0330

    feat: add swagger doc

---
+++ src/core/config/swagger/constants.ts
@@ -0,0 +1,4 @@
+export const SWAGGER_API_ROOT = 'api_doc';
+export const SWAGGER_API_NAME = 'network_utilities APP';
+export const SWAGGER_API_DESCRIPTION = 'network_utilities API';
+export const SWAGGER_API_CURRENT_VERSION = '1.0';
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as ping from 'ping';
+import { setupSwagger } from './core/config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
