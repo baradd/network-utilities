1fd527c06d7e0dd278c242583ac2d47a64a6f553
baradd <mohammadtavassolian3@gmail.com>
2026-06-17 01:57:09 +0330

    fix: load entities

--- src/core/database/sqlite.module.ts
+++ src/core/database/sqlite.module.ts
@@ -5,6 +5,8 @@ import sqliteConfig from '../config/database/sqlite/sqlite.config';
 import { SqliteConfigService } from '../config/database/sqlite/sqlite-config.service';
 import { SqliteDataSource } from '../config/database/sqlite/datasource';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { User } from 'src/modules/users/entities/user.entity';
+import { TelegramProfile } from 'src/modules/telegram/entities/telegram-profile.entity';
 
 @Module({
   imports: [
@@ -15,17 +17,18 @@ import { DataSource, DataSourceOptions } from 'typeorm';
       useFactory: (config: SqliteConfigService) => ({
         type: 'better-sqlite3',
         database: config.dbName,
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
         autoLoadEntities: true,
       }),
       async dataSourceFactory(options?: DataSourceOptions) {
-        if (!options) {
-          throw new Error('DataSource options are undefined');
-        }
+        if (!options) throw new Error('DataSource options are undefined');
+
         const datasource = new DataSource(options);
+        await datasource.initialize();
+
         const { database } = datasource.options as any;
-        console.log(`✅ Connected to DB: ${database}}`);
+        console.log(`✅ Connected to DB: ${database}`);
+
         return datasource;
       },
     }),
